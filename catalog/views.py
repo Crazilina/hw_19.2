7187eaf38a9c65a2191e97b1542fbe094df2acc5
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-04-22 23:57:55 +0200

    HW_21.1: Completed task 1.

--- catalog/views.py
+++ catalog/views.py
@@ -1,44 +1,36 @@
+from django.views.generic import ListView, DetailView, View
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import HttpResponse
-
-from catalog.models import Product
+from .models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/home.html', context)
+class HomeListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
 
-@ensure_csrf_cookie
-def contacts(request):
-    if request.method == 'POST':
-        # Если запрос - POST, значит пользователь отправил данные формы
+class ContactsView(View):
+    @staticmethod
+    def get(request):
+        return render(request, 'catalog/contacts.html')
+
+    @ensure_csrf_cookie
+    def post(self, request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-        # Теперь можно обрабатывать данные формы, например, отправить их на почту или сохранить в базе данных
+        # Обработка данных формы
 
-        # В данном примере просто выведем данные на экран
         print(f"Пользователь {name} с email {email} отправил следующее сообщение:")
         print(message)
 
-        # Далее можно добавить код для отправки письма администратору или сохранения данных в базу данных
-
-        # После обработки формы, можно перенаправить пользователя
-        # на страницу "Спасибо за обратную связь" или что-то подобное
         return HttpResponse("Спасибо за обратную связь!")
-    else:
-        # Если запрос - GET, просто показываем страницу контактов
-        return render(request, 'catalog/contacts.html')
 
 
-def product_detail(request, pk):
-    # Получаем объект товара по его primary key
-    product = Product.objects.get(pk=pk)
-    # Формируем контекст для передачи в шаблон
-    context = {'product': product}
-    # Отображаем шаблон с переданным контекстом
-    return render(request, 'catalog/product_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
